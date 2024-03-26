d557528549c3314da5f83941ddb71df0f399ed26
Victoriia <viktoriia.paster@beetroot.se>
2024-03-26 14:10:41 +0200

    ADD | Star Rating component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 import { useState } from "react";
 import { Nav } from "./Nav";
-import { tempMovieData } from './data';
+import { tempMovieData } from "./data";
 import { Main } from "./Main";
 import { Search } from "./Search";
 import { Logo } from "./Logo";
 import { NumFilm } from "./NumFilms";
-import { Box } from './ListBox';
+import { Box } from "./ListBox";
 import { FilmList } from "./FilmList";
-import {tempWatchedData} from './data';
+import { tempWatchedData } from "./data";
 import { WatchedSummary } from "./WatchedSummary";
 import { WatchedList } from "./WatchedList";
+import { StarRating } from "./Star";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
 
-  
+  function MovieRating() {
+    const [movieRating, setMovieRating] = useState(0);
+    return (
+      <div>
+        <StarRating color="purple" onSetRating={setMovieRating} />
+        <p>This moview was rates {movieRating} stars</p>
+      </div>
+    );
+  }
+
   return (
     <>
-      <Nav> 
+      <Nav>
         <Logo />
-        <Search/>
-        <NumFilm movies={movies}/>
+        <Search />
+        <NumFilm movies={movies} />
       </Nav>
       <Main>
         <Box>
-          <FilmList movies={movies} /> 
-        </Box> 
+          <FilmList movies={movies} />
+        </Box>
         <Box>
-          <WatchedSummary watched={watched}/>
-          <WatchedList watched={watched}/>
-        </Box> 
-    </Main>
+          <WatchedSummary watched={watched} />
+          <WatchedList watched={watched} />
+          <StarRating maxRating={10} />
+          <StarRating
+            size={20}
+            color="green"
+            className="test-star"
+            messages={["Terrible", "Bad", "Normal", "Good", "Amazing"]}
+            defaultValue={1}
+          />
+          <MovieRating />
+        </Box>
+      </Main>
     </>
   );
 }
