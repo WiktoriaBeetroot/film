c84e84eb784ad7930e82a9a0831fce2a51890c4c
Victoriia <viktoriia.paster@beetroot.se>
2024-09-04 18:05:34 +0300

    ADD | ref for search input logic

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,26 @@
+import { useEffect, useRef } from "react";
+
+
 export const Search = ({query, setQuery}) => {
+  const inputEl = useRef(null);
+
+  useEffect(function() {
+
+    function callback(e) {
+
+      if(document.activeElement === inputEl.current) return
+
+      if (e.code === 'Enter') {
+        inputEl.current.focus();
+        setQuery('');
+      }
+    }
+
+    document.addEventListener('keydown', callback);
+
+    return ()=> document.addEventListener('keydown', callback)
+  }, [setQuery])
+
 
     return (
         <input
@@ -7,6 +29,7 @@ export const Search = ({query, setQuery}) => {
         placeholder="Search movies..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputEl}
       />
     )
 }
